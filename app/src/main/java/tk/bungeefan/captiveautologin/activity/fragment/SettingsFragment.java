0d1e6284b9862ebc40956b037b8fba71d57fe071
Bungeefan <severin.hamader@yahoo.de>
2022-02-16 21:47:39 +0100

    Added Network Mismatch Warning Option

--- app/src/main/java/tk/bungeefan/captiveautologin/activity/fragment/SettingsFragment.java
+++ app/src/main/java/tk/bungeefan/captiveautologin/activity/fragment/SettingsFragment.java
@@ -1,28 +1,57 @@
 package tk.bungeefan.captiveautologin.activity.fragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import tk.bungeefan.captiveautologin.R;
+import tk.bungeefan.captiveautologin.activity.SettingsActivity;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    private static final SharedPreferences.OnSharedPreferenceChangeListener CHANGE_LISTENER = (prefs, key) -> {
+        if (key.equals(SettingsActivity.THEME_KEY)) {
+            SettingsActivity.setTheme(prefs);
+        }
+    };
+
     @Override
     public void onCreatePreferences(Bundle bundle, String rootKey) {
-        // Load the Preferences from the XML file
         setPreferencesFromResource(R.xml.app_preferences, rootKey);
 
-        Preference button = findPreference("notificationBtn");
-        button.setOnPreferenceClickListener(preference -> {
-            Intent intent = new Intent()
-                    .setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
-                    .putExtra(Settings.EXTRA_APP_PACKAGE, getContext().getPackageName())
-                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            return true;
-        });
+        ListPreference themePreference = findPreference("pref_theme");
+        if (themePreference != null) {
+            themePreference.setSummaryProvider((Preference.SummaryProvider<ListPreference>) pref -> pref.getEntry());
+        }
+
+        Preference notificationsLink = findPreference("notifications");
+        if (notificationsLink != null) {
+            notificationsLink.setOnPreferenceClickListener(pref -> {
+                Intent intent = new Intent()
+                        .setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS)
+                        .putExtra(Settings.EXTRA_APP_PACKAGE, getContext().getPackageName())
+                        .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+            });
+        }
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(CHANGE_LISTENER);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(CHANGE_LISTENER);
+    }
+
 }
