500a34d07117d83228b4f6cf6458082f7ee675a1
Bungeefan <severin.hamader@yahoo.de>
2022-02-24 21:07:57 +0100

    Added dynamic wifi scan results

--- app/src/main/java/tk/bungeefan/captiveautologin/activity/fragment/AddWifiDialogFragment.java
+++ app/src/main/java/tk/bungeefan/captiveautologin/activity/fragment/AddWifiDialogFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.pm.PackageManager;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -34,6 +35,7 @@ public class AddWifiDialogFragment extends DialogFragment {
     public static final String BUNDLE_KEY = "loginData";
     private ArrayAdapter<String> mWifiSpinnerAdapter;
     private WifiManager mWifiManager;
+    private WifiManager.ScanResultsCallback scanResultCallback;
 
     public static AddWifiDialogFragment newInstance(@Nullable Login login) {
         AddWifiDialogFragment dialogFragment = new AddWifiDialogFragment();
@@ -53,21 +55,33 @@ public class AddWifiDialogFragment extends DialogFragment {
     }
 
     private void loadNetworks() {
-        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        List<String> configuredNetworks = null;
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            List<WifiConfiguration> wifiConfigurations = mWifiManager.getConfiguredNetworks();
+            if (wifiConfigurations != null && !wifiConfigurations.isEmpty()) {
+                configuredNetworks = wifiConfigurations.stream()
+                        .map(wifiConfiguration -> wifiConfiguration.SSID)
+                        .collect(Collectors.toList());
+            }
         }
 
-        List<WifiConfiguration> wifiConfigurations = mWifiManager.getConfiguredNetworks();
-        List<String> configuredNetworks = (wifiConfigurations != null && !wifiConfigurations.isEmpty() ?
-                wifiConfigurations.stream()
-                        .map(wifiConfiguration -> Util.replaceSSID(wifiConfiguration.SSID)) :
-                mWifiManager.getScanResults().stream()
-                        .map(scanResult -> Util.replaceSSID(scanResult.SSID))
-                        .filter(s -> !s.isEmpty())
-                        .distinct())
-                .sorted()
-                .collect(Collectors.toList());
-        mWifiSpinnerAdapter.addAll(configuredNetworks);
+        if (configuredNetworks == null) {
+            configuredNetworks = mWifiManager.getScanResults().stream()
+                    .map(scanResult -> scanResult.SSID)
+                    .collect(Collectors.toList());
+        }
+
+        if (!configuredNetworks.isEmpty()) {
+            mWifiSpinnerAdapter.clear();
+            mWifiSpinnerAdapter.addAll(configuredNetworks.stream()
+                    .map(Util::replaceSSID)
+                    .filter(s -> !s.isEmpty())
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.toList())
+            );
+        }
     }
 
     @NonNull
@@ -83,48 +97,71 @@ public class AddWifiDialogFragment extends DialogFragment {
 
         wifiSpinner.setAdapter(mWifiSpinnerAdapter);
 
+        Login login = getArguments() != null ? (Login) getArguments().getSerializable(WifiDetailsFragment.DATA_KEY) : null;
+        if (login != null) {
+            ssidField.setText(login.getSSID());
+            usernameField.setText(login.getUsername());
+            passwordField.setText(login.getPassword());
+        } else if (!Util.isUnknownSSID(mWifiManager.getConnectionInfo().getSSID())) {
+            String ssid = Util.replaceSSID(mWifiManager.getConnectionInfo().getSSID());
+            ssidField.setText(ssid);
+        }
+
         SwitchMaterial manualInputSwitch = view.findViewById(R.id.manualInputSwitch);
         manualInputSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked && ssidField.getText().toString().isEmpty()) {
+                String selectedItem = (String) wifiSpinner.getSelectedItem();
+                if (selectedItem != null) {
+                    ssidField.setText(selectedItem);
+                }
+            }
             wifiSpinner.setVisibility(isChecked ? View.GONE : View.VISIBLE);
             input_layout_ssid.setVisibility(isChecked ? View.VISIBLE : View.GONE);
         });
 
-        if (mWifiSpinnerAdapter.isEmpty() && Util.isUnknownSSID(mWifiManager.getConnectionInfo().getSSID())) {
-            manualInputSwitch.setChecked(true);
+        if (mWifiSpinnerAdapter.isEmpty()) {
             manualInputSwitch.setEnabled(false);
-        }
-
-        Login login = getArguments() != null ? (Login) getArguments().getSerializable(WifiDetailsFragment.DATA_KEY) : null;
-        if (login != null) {
-            usernameField.setText(login.getUsername());
-            passwordField.setText(login.getPassword());
-
-            int position = mWifiSpinnerAdapter.getPosition(login.getSSID());
-            if (position == -1) {
-                manualInputSwitch.setChecked(true);
-            }
-
-            if (!manualInputSwitch.isChecked()) {
+            manualInputSwitch.setChecked(true);
+        } else if (login != null) {
+            manualInputSwitch.setChecked(true);
+        } else {
+            int position = mWifiSpinnerAdapter.getPosition(ssidField.getText().toString());
+            if (position != -1) {
                 wifiSpinner.setSelection(position);
-            } else {
-                ssidField.setText(login.getSSID());
-            }
-        } else if (!Util.isUnknownSSID(mWifiManager.getConnectionInfo().getSSID())) {
-            String ssid = Util.replaceSSID(mWifiManager.getConnectionInfo().getSSID());
-
-            int position = mWifiSpinnerAdapter.getPosition(ssid);
-            if (position == -1) {
-                manualInputSwitch.setChecked(true);
-            }
-
-            if (!manualInputSwitch.isChecked()) {
-                wifiSpinner.setSelection(position);
-            } else {
-                ssidField.setText(ssid);
             }
         }
 
-        return new MaterialAlertDialogBuilder(getActivity())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            scanResultCallback = new WifiManager.ScanResultsCallback() {
+
+                @Override
+                public void onScanResultsAvailable() {
+                    String selectedItem = (String) wifiSpinner.getSelectedItem();
+                    loadNetworks();
+
+                    manualInputSwitch.setChecked(mWifiSpinnerAdapter.isEmpty());
+                    manualInputSwitch.setEnabled(!mWifiSpinnerAdapter.isEmpty());
+
+                    if (!mWifiSpinnerAdapter.isEmpty()) {
+                        if (selectedItem != null) {
+                            int position = mWifiSpinnerAdapter.getPosition(selectedItem);
+                            if (position != -1) {
+                                wifiSpinner.setSelection(position);
+                                return;
+                            }
+                        }
+
+                        String ssid = Util.replaceSSID(mWifiManager.getConnectionInfo().getSSID());
+                        int position = mWifiSpinnerAdapter.getPosition(ssid);
+                        if (position != -1) {
+                            wifiSpinner.setSelection(position);
+                        }
+                    }
+                }
+            };
+        }
+
+        return new MaterialAlertDialogBuilder(requireContext())
                 .setTitle(login == null ? getString(R.string.add_login_title) : getString(R.string.edit_login_title))
                 .setView(view)
                 .setPositiveButton(android.R.string.ok, (dialog, which) -> {
@@ -143,5 +180,27 @@ public class AddWifiDialogFragment extends DialogFragment {
                 })
                 .setNegativeButton(android.R.string.cancel, null)
                 .create();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                if (scanResultCallback != null) {
+                    mWifiManager.registerScanResultsCallback(requireContext().getMainExecutor(), scanResultCallback);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (scanResultCallback != null) {
+                mWifiManager.unregisterScanResultsCallback(scanResultCallback);
+            }
+        }
     }
 }
