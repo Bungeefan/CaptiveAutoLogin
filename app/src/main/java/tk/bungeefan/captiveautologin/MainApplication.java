9415b334e4f16607572c36238a6178b7c073ca6d
Bungeefan <severin.hamader@yahoo.de>
2022-06-29 21:36:48 +0200

    Added ACRA

--- app/src/main/java/tk/bungeefan/captiveautologin/MainApplication.java
+++ app/src/main/java/tk/bungeefan/captiveautologin/MainApplication.java
@@ -1,6 +1,7 @@
 package tk.bungeefan.captiveautologin;
 
 import android.app.Application;
+import android.content.Context;
 import android.os.Build;
 import android.os.StrictMode;
 
@@ -8,12 +9,25 @@ import androidx.preference.PreferenceManager;
 
 import com.google.android.material.color.DynamicColors;
 
+import org.acra.ACRA;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.MailSenderConfigurationBuilder;
+import org.acra.config.NotificationConfigurationBuilder;
+import org.acra.data.StringFormat;
+
 import tk.bungeefan.captiveautologin.activity.SettingsActivity;
 
 public class MainApplication extends Application {
 
+    private static final String REPORT_MAIL = "severin.hamader@yahoo.de";
+
     @Override
     public void onCreate() {
+        if (ACRA.isACRASenderServiceProcess()) {
+            super.onCreate();
+            return;
+        }
+
         if (BuildConfig.DEBUG) {
             StrictMode.enableDefaults();
         }
@@ -26,6 +40,32 @@ public class MainApplication extends Application {
         }
 
         super.onCreate();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        ACRA.init(this, new CoreConfigurationBuilder()
+                .withBuildConfigClass(BuildConfig.class)
+                .withReportFormat(StringFormat.JSON)
+                .withPluginConfigurations(
+                        new MailSenderConfigurationBuilder()
+                                .withMailTo(REPORT_MAIL)
+                                .withReportAsFile(true)
+                                .build(),
+                        new NotificationConfigurationBuilder()
+                                .withTitle(getString(R.string.notification_title_crash))
+                                .withText(getString(R.string.notification_text_crash))
+                                .withChannelName(getString(R.string.notification_channel_crash))
+                                .withTickerText(getString(R.string.notification_text_crash))
+                                .withResIcon(R.drawable.ic_stat_name)
+                                .withSendButtonText(getString(android.R.string.ok))
+                                .withDiscardButtonText(getString(android.R.string.cancel))
+                                .withSendOnClick(true)
+                                .build()
+                )
+        );
     }
 
 }
